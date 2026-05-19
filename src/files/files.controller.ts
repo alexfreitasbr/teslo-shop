994767df73248a0b8a183479dc63c07b3f5213f8
alexfreitasbr <alexfreitasbr@hotmail.com>
2026-05-19 11:25:39 -0300

    upload

---
+++ src/files/files.controller.ts
@@ -0,0 +1,23 @@
+import { BadGatewayException, BadRequestException, Controller, Post, UploadedFile, UseInterceptors} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/fileFilter.helpers';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors( FileInterceptor('file', {
+    fileFilter: fileFilter,
+    // limits: { fileSize: 1000 },
+    // dest: './files',
+  }))
+  uploadProductImage(
+    @UploadedFile() file: Express.Multer.File) {
+
+    if(!file) throw new BadRequestException('Make sure that the file is an image');
+    return {fileName: file.originalname};
+  }
+
+}
